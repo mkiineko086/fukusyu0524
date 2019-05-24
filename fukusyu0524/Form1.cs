899c9177c513ac71f5f522bb7dcd8c3525ba88f3
mkiineko086 <49701911+mkiineko086@users.noreply.github.com>
2019-05-24 13:24:39 +0900

    復習です

--- fukusyu0524/Form1.cs
+++ fukusyu0524/Form1.cs
@@ -12,9 +12,48 @@ namespace fukusyu0524
 {
     public partial class Form1 : Form
     {
+        int vx = 0;
+        int vy = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vx = 0;
+            vy = -10;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            vx = 0;
+            vy = +10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vx = -10;
+            vy = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            vx = +10;
+            vy = 0;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            vx = 0;
+            vy = 0;
         }
     }
 }
